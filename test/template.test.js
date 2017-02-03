c9528f609b02561eeac98382aac3878248e08960
gretacb <carol@mapbox.com>
2017-02-03 12:17:29 -0500

    update based on new tilejson schema

--- test/template.test.js
+++ test/template.test.js
@@ -10,34 +10,34 @@ tape('templateToTileJSON', function(assert) {
     assert.equal(TileJSON.templateToTileJSON('http://www.example.com'), false, 'not zxy url');
     assert.equal(TileJSON.templateToTileJSON('http://www.example.com/{z}'), false, 'not zxy url');
     assert.deepEqual(TileJSON.templateToTileJSON('http://www.example.com/{z}/{x}/{y}.png'), {
-        tilejson: '2.1.0',
+        tilejson: '2.2.0',
         minzoom: 0,
-        maxzoom: 22,
+        maxzoom: 30,
         center: [0,0,3],
         bounds: [-180,-85.0511,180,85.0511],
         geocoder_data: 'http://www.example.com',
         tiles: [ 'http://www.example.com/{z}/{x}/{y}.png' ]
     }, 'returns tilejson with templated url');
     assert.deepEqual(TileJSON.templateToTileJSON('http://www.example.com/{z}-{x}-{y}.png'), {
-        tilejson: '2.1.0',
+        tilejson: '2.2.0',
         minzoom: 0,
-        maxzoom: 22,
+        maxzoom: 30,
         center: [0,0,3],
         bounds: [-180,-85.0511,180,85.0511],
         tiles: [ 'http://www.example.com/{z}-{x}-{y}.png' ]
     }, 'returns tilejson with templated url');
     assert.deepEqual(TileJSON.templateToTileJSON('http://www.example.com/{z}/{x}/{y}.grid.json'), {
-        tilejson: '2.1.0',
+        tilejson: '2.2.0',
         minzoom: 0,
-        maxzoom: 22,
+        maxzoom: 30,
         center: [0,0,3],
         bounds: [-180,-85.0511,180,85.0511],
         grids: [ 'http://www.example.com/{z}/{x}/{y}.grid.json' ]
     }, 'returns tilejson with templated url');
     assert.deepEqual(TileJSON.templateToTileJSON('tilejson+http://www.example.com/{z}/{x}/{y}.png'), {
-        tilejson: '2.1.0',
+        tilejson: '2.2.0',
         minzoom: 0,
-        maxzoom: 22,
+        maxzoom: 30,
         center: [0,0,3],
         bounds: [-180,-85.0511,180,85.0511],
         geocoder_data: 'http://www.example.com',
